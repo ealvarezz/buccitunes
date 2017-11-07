694c09f07ac35f8a6db74660fce5eeb17f232cc3
JustJaw <datcloud@me.com>
2017-11-06 21:05:55 -0500

    More classes

--- src/main/java/com/buccitunes/model/RequestedAlbum.java
+++ src/main/java/com/buccitunes/model/RequestedAlbum.java
@@ -1,5 +1,114 @@
 package com.buccitunes.model;
 
-public class RequestedAlbum {
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class RequestedAlbum {
+	private int id;
+	
+	private String biography;
+	
+	private String coverArtPath;
+	
+	private List<Artist> featuredArtists;
+	
+	private boolean isASingle;
+	
+	@DateTimeFormat(pattern="MM/dd/yyyy")
+	private Date releaseDate;
+	
+	private String label;
+	
+	private List<Genre> genres;
+	
+	private List<RequestedSong> songs;
+	
+	private String comments;
+	
+	private ArtistUser user;
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+	public String getCoverArtPath() {
+		return coverArtPath;
+	}
+
+	public void setCoverArtPath(String coverArtPath) {
+		this.coverArtPath = coverArtPath;
+	}
+
+	public List<Artist> getFeaturedArtists() {
+		return featuredArtists;
+	}
+
+	public void setFeaturedArtists(List<Artist> featuredArtists) {
+		this.featuredArtists = featuredArtists;
+	}
+
+	public boolean isASingle() {
+		return isASingle;
+	}
+
+	public void setASingle(boolean isASingle) {
+		this.isASingle = isASingle;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public List<Genre> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(List<Genre> genres) {
+		this.genres = genres;
+	}
+
+	public List<RequestedSong> getSongs() {
+		return songs;
+	}
+
+	public void setSongs(List<RequestedSong> songs) {
+		this.songs = songs;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public ArtistUser getUser() {
+		return user;
+	}
+
+	public void setUser(ArtistUser user) {
+		this.user = user;
+	}
+	
+	
+	
 }
--- src/main/java/com/buccitunes/model/RequestedSong.java
+++ src/main/java/com/buccitunes/model/RequestedSong.java
@@ -1,5 +1,110 @@
 package com.buccitunes.model;
 
-public class RequestedSong {
+import java.util.List;
 
+public class RequestedSong {
+	private int id;
+	
+	private String name;
+	
+	private int duration;
+	
+	private Artist artist;
+	
+	private List<Artist> features;
+	
+	private String audioPath;
+	
+	private MimeType mimeType;
+	
+	private List<Genre> genres;
+	
+	private String picturePath;
+	
+	private String comments;
+	
+	private ArtistUser requester;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public Artist getArtist() {
+		return artist;
+	}
+
+	public void setArtist(Artist artist) {
+		this.artist = artist;
+	}
+
+	public List<Artist> getFeatures() {
+		return features;
+	}
+
+	public void setFeatures(List<Artist> features) {
+		this.features = features;
+	}
+
+	public String getAudioPath() {
+		return audioPath;
+	}
+
+	public void setAudioPath(String audioPath) {
+		this.audioPath = audioPath;
+	}
+
+	public MimeType getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(MimeType mimeType) {
+		this.mimeType = mimeType;
+	}
+
+	public List<Genre> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(List<Genre> genres) {
+		this.genres = genres;
+	}
+
+	public String getPicturePath() {
+		return picturePath;
+	}
+
+	public void setPicturePath(String picturePath) {
+		this.picturePath = picturePath;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public ArtistUser getRequester() {
+		return requester;
+	}
+
+	public void setRequester(ArtistUser requester) {
+		this.requester = requester;
+	}
+	
+	//private RequestedAlbum albumRequestedOn;
+ 
 }
