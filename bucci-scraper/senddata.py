2900839b8acd1ddf1f948a68335fb43d8d248c5f
JustJaw <justin.williams.2@stonybrook.edu>
2017-11-17 03:27:58 -0500

    Some fixes made such as the endpoints

--- bucci-scraper/senddata.py
+++ bucci-scraper/senddata.py
@@ -4,11 +4,12 @@ import json
 
 PATH = "./bucci/"
 
-ARTIST_ENDPOINT = "http://localhost/addArtist"
-ALBUM_ENDPOINT = "http://localhost/approveAlbum"
+ARTIST_ENDPOINT = "http://localhost:8080/addArtistUser"
+ALBUM_ENDPOINT = "http://localhost:8080/approveAlbum"
 
 
 #try:
+print("\n==============Creating Artists==============\n")
 artpath = PATH+"artist/"
 for i in os.listdir(artpath):
     print(artpath+i)
@@ -18,9 +19,10 @@ for i in os.listdir(artpath):
 #    print("shit")
 
 #try:
+print("\n==============Creating Albums==============\n")
 albpath = PATH+"album/"
 for i in os.listdir(albpath):
-    print(artpath+i)
+    print(albpath+i)
     artist_obj = json.loads(open(albpath+i, 'r').read())
     r = requests.post(ALBUM_ENDPOINT, json=artist_obj)
 #except:
