e94677b519c5b6109d9372c8f982f98079c15c11
JustJaw <justin.williams.2@stonybrook.edu>
2017-11-19 19:44:22 -0500

    Edits to Script

--- bucci-scraper/playSongs.py
+++ bucci-scraper/playSongs.py
@@ -6,9 +6,9 @@ import json
 
 PLAY_ENDPOINT = "http://localhost:8080/playsong"
 
-users = ['1610@fake.com', '69@fake.com', '302@fake.com']
+users = ['1610@fake.com', '69@fake.com', '302@fake.com', '841@fake.com']
 songsPlayed = [1, 4, 6, 9, 20, 100, 14, 11, 9, 12, 100, 400, 600, 900, 200, 100, 140, 110, 903, 120]
-i = 0
+i = len(songsPlayed) - 1
 
 for user in users:
     for played in songsPlayed:
@@ -18,4 +18,4 @@ for user in users:
     requestURL = PLAY_ENDPOINT + "?userId=" + user + "&songId=" + str(songsPlayed[i])
     r = requests.get(requestURL)
     print(user + " has played " + r.json()['response']['name'])
-    i += 1
+    i -= 1
